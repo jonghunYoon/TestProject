2db7a3313c661c7f5e7798259f39795480433bd5
jonghunYoon <yjhchic@gmail.com>
2016-03-19 16:56:49 +0900

    First

---
+++ Assets/Script/Debugging.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debugging : MonoBehaviour {
+
+    private void Awake()
+    {
+        int from = 0;
+        int to = 100000000;
+
+        for(float factor = 0.0f; factor <= 1.0f; factor += 0.1f)
+        {
+            int result = (int)(from * (1.0f - factor) + to * factor);
+
+            DevDebug.Log.Normal(result);
+        }
+    }
+}
---
+++ Assets/Script/DevDebug.cs
@@ -0,0 +1,52 @@
+﻿#define DEV_DEBUG
+
+using UnityEngine;
+using System.Collections;
+
+public class DevDebug
+{
+    public class Log
+    {
+        [System.Diagnostics.Conditional("DEV_DEBUG")]
+        public static void Normal(System.Object msg)
+        {
+            UnityEngine.Debug.Log(Frame() + msg);
+        }
+
+        public static void Warrning(System.Object msg)
+        {
+            UnityEngine.Debug.Log(Frame() + msg);
+        }
+
+        public static void Error(System.Object msg)
+        {
+            UnityEngine.Debug.Log(Frame() + msg);
+        }
+        public static string Frame()
+        {
+            return string.Format("[{0}] : ", Time.frameCount);
+        }
+    }
+
+    public class Draw
+    {
+        public static void Line(Vector3 start, Vector3 end, Color color, float duration)
+        {
+            Debug.DrawLine(start, end, color, duration);
+        }
+    }
+    public static void Assert(UnityEngine.Object obj)
+    {
+        if(null == obj)
+        {
+            var stackTrace = new System.Diagnostics.StackTrace();
+            var frame = stackTrace.GetFrame(1);
+
+            string message = "[!!!](Assert)[!!!] : " + frame.GetMethod().Name +
+            "( " + frame.GetFileName() + " [line : " +
+            frame.GetFileLineNumber() + " ] )";
+
+        }
+    }
+
+}
